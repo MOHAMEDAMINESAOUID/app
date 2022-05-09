1035002e2b51efadf440c78c10c063b43a7e0a7b
Saouid <saouidmohamedamine@emsi-edu.ma>
2022-05-09 11:46:51 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Search from './Components/Search';
+import Search2 from './Components/Search2';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style ={styles.main_container}>
+    <Search2 />
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main_container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
   },
-});
+})
+
